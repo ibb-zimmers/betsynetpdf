0ad476d1d8410f0d7427f36c4612a965f7a8774a
ibb-zimmers <zimmer@betsy.de>
2015-03-13 14:31:14 +0100

    changed project home from google code to github

--- BetsyNetPDF/BetsyNetPDFWrapperUI/AboutDialog.Designer.cs
+++ BetsyNetPDF/BetsyNetPDFWrapperUI/AboutDialog.Designer.cs
@@ -82,10 +82,10 @@
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Location = new System.Drawing.Point(9, 72);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(203, 13);
+            this.linkLabel1.Size = new System.Drawing.Size(220, 13);
             this.linkLabel1.TabIndex = 3;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "https://code.google.com/p/betsynetpdf/";
+            this.linkLabel1.Text = "https://github.com/ibb-zimmers/betsynetpdf";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_LinkClicked);
             // 
             // memoEdit1
--- BetsyNetPDF/BetsyNetPDFWrapperUIWF/AboutDialog.Designer.cs
+++ BetsyNetPDF/BetsyNetPDFWrapperUIWF/AboutDialog.Designer.cs
@@ -93,10 +93,10 @@
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Location = new System.Drawing.Point(13, 55);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(201, 13);
+            this.linkLabel1.Size = new System.Drawing.Size(214, 13);
             this.linkLabel1.TabIndex = 4;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "https://code.google.com/p/betsynetpdf/";
+            this.linkLabel1.Text = "https://github.com/ibb-zimmers/betsynetpdf";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_LinkClicked);
             // 
             // textBox1
