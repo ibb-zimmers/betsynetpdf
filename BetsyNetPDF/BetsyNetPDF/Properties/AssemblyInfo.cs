a381b446a56b52e6d72c5a2264343d15ccf4d2e2
ibb-zimmers <zimmer@betsy.de>
2015-03-17 08:25:30 +0100

    new versioning scheme
    1.YY.MDD.AutoInc

--- BetsyNetPDF/BetsyNetPDF/Properties/AssemblyInfo.cs
+++ BetsyNetPDF/BetsyNetPDF/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.15.316.20")]
+[assembly: AssemblyFileVersion("1.15.316.20")]
--- BetsyNetPDF/BetsyNetPDFWrapperUI/Properties/AssemblyInfo.cs
+++ BetsyNetPDF/BetsyNetPDFWrapperUI/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2015.3.16")]
-[assembly: AssemblyFileVersion("1.2015.3.16")]
+[assembly: AssemblyVersion("1.15.316.36")]
+[assembly: AssemblyFileVersion("1.15.316.36")]
