83ac63dd85d794a81b5693fdab2787191d5debc9
ibb-zimmers <zimmer@betsy.de>
2016-02-29 15:46:11 +0100

    updated copyright info

--- BetsyNetPDF/BetsyNetPDF/Properties/AssemblyInfo.cs
+++ BetsyNetPDF/BetsyNetPDF/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.15.1117.35")]
-[assembly: AssemblyFileVersion("1.15.1117.35")]
+[assembly: AssemblyVersion("1.16.229.36")]
+[assembly: AssemblyFileVersion("1.16.229.36")]
